3a67f6f3ad83e78da992f3e1f99fa5670ce26924
Johann <johann.hugon@pm.me>
2025-02-04 17:46:22 -0600

    Add init and first test

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,26 @@
+pub mod protocols;
+use crate::protocols::ipv4::Ipv4Header;
+use pnet::packet::ipv4::Ipv4Packet;
+
+use pnet::packet::ethernet::{EthernetPacket,EtherTypes};
+
+pub struct Nprint{
+    pub ipv4:Option<Ipv4Header>,
+}
+
+impl Nprint{
+    pub fn new(packet:&[u8])-> Nprint{
+        let ethernet = EthernetPacket::new(&packet).unwrap();
+        if ethernet.get_ethertype() == EtherTypes::Ipv4{
+            Nprint{
+                ipv4:Some(Ipv4Header::new(Ipv4Packet::new(packet).unwrap()))
+            }           
+        } else {
+            todo!()
+        }
+    }
+}
+
 pub fn hello_world() -> String {
     "hello_world".to_string()
 }
