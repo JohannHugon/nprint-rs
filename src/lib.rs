5081a7663fc4ef785b29416cc6e83e5ca8b4b8fc
Johann <107463949+JohannHugon@users.noreply.github.com>
2025-02-05 11:04:25 -0600

    1 first working parsing (#2)
    * Ipv4 parsing

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,23 @@
-pub fn hello_world() -> String {
-    "hello_world".to_string()
+pub mod protocols;
+use crate::protocols::ipv4::Ipv4Header;
+use pnet::packet::ipv4::Ipv4Packet;
+use pnet::packet::Packet;
+use pnet::packet::ethernet::{EthernetPacket,EtherTypes};
+
+#[derive(Debug)]
+pub struct Nprint{
+    pub ipv4:Option<Ipv4Header>,
 }
 
+impl Nprint{
+    pub fn new(packet:&[u8])-> Nprint{
+        let ethernet = EthernetPacket::new(&packet).unwrap();
+        if ethernet.get_ethertype() == EtherTypes::Ipv4{
+            Nprint{
+                ipv4:Some(Ipv4Header::new(Ipv4Packet::new(ethernet.payload()).unwrap()))
+            }           
+        } else {
+            todo!()
+        }
+    }
+}
