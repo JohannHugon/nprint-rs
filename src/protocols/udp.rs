2da4f45cf83ae5b3138f8d529d9d5ee91e96ae9e
Johann <107463949+JohannHugon@users.noreply.github.com>
2025-02-08 19:56:49 -0600

    10 setup GitHub action (#15)

--- src/protocols/udp.rs
+++ src/protocols/udp.rs
@@ -1,29 +1,43 @@
 use pnet::packet::udp::UdpPacket;
 
-#[derive(Clone,Debug)]
-pub struct UdpHeader{
+#[derive(Clone, Debug)]
+pub struct UdpHeader {
     data: Vec<i8>,
 }
 
-impl UdpHeader{
-    pub fn new(packet: &UdpPacket) -> UdpHeader{
+impl UdpHeader {
+    pub fn new(packet: &UdpPacket) -> UdpHeader {
         let mut data = Vec::new();
-        data.extend((0..16).rev().map(|i| ((packet.get_source() >> i) & 1) as i8));
-        data.extend((0..16).rev().map(|i| ((packet.get_destination() >> i) & 1) as i8));
-        data.extend((0..16).rev().map(|i| ((packet.get_length() >> i) & 1) as i8));
-        data.extend((0..16).rev().map(|i| ((packet.get_checksum() >> i) & 1) as i8));
-        UdpHeader{
-            data,
-        }
+        data.extend(
+            (0..16)
+                .rev()
+                .map(|i| ((packet.get_source() >> i) & 1) as i8),
+        );
+        data.extend(
+            (0..16)
+                .rev()
+                .map(|i| ((packet.get_destination() >> i) & 1) as i8),
+        );
+        data.extend(
+            (0..16)
+                .rev()
+                .map(|i| ((packet.get_length() >> i) & 1) as i8),
+        );
+        data.extend(
+            (0..16)
+                .rev()
+                .map(|i| ((packet.get_checksum() >> i) & 1) as i8),
+        );
+        UdpHeader { data }
     }
-    pub fn get_data(&self)->&Vec<i8>{
+    pub fn get_data(&self) -> &Vec<i8> {
         &self.data
     }
-    pub fn remove(&mut self,start: usize,end:usize){
+    pub fn remove(&mut self, start: usize, end: usize) {
         self.data[start..=end].fill(0);
     }
     pub fn get_headers() -> Vec<String> {
-        let fields = vec![
+        let fields = [
             ("udp_sport", 16),
             ("udp_dport", 16),
             ("udp_len", 16),
@@ -36,4 +50,3 @@ impl UdpHeader{
             .collect()
     }
 }
-
