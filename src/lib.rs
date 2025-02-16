56d9ec579cc980d05ef45bb40139a812d611bbf9
Johann <107463949+JohannHugon@users.noreply.github.com>
2025-02-16 16:18:14 -0600

    36 more than one packet (#37)

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,12 @@ use pnet::packet::Packet;
 
 #[derive(Debug)]
 pub struct Nprint {
+    pub data: Vec<Headers>,
+    protocols: Vec<Protocol>,
+}
+
+#[derive(Debug)]
+pub struct Headers {
     pub ipv4: Option<Ipv4Header>,
     pub tcp: Option<TcpHeader>,
     pub udp: Option<UdpHeader>,
@@ -27,6 +33,35 @@ pub enum Protocol {
 
 impl Nprint {
     pub fn new(packet: &[u8], protocols: Vec<Protocol>) -> Nprint {
+        Nprint {
+            data: vec![Headers::new(packet, &protocols)],
+            protocols,
+        }
+    }
+
+    pub fn print(&self) -> Vec<f32> {
+        let mut output = vec![];
+        for header in &self.data {
+            if let Some(ipv4) = &header.ipv4 {
+                output.extend(ipv4.get_data());
+            }
+            if let Some(tcp) = &header.tcp {
+                output.extend(tcp.get_data());
+            }
+            if let Some(udp) = &header.udp {
+                output.extend(udp.get_data());
+            }
+        }
+        output
+    }
+
+    pub fn add(&mut self, packet: &[u8]) {
+        self.data.push(Headers::new(packet, &self.protocols));
+    }
+}
+
+impl Headers {
+    pub fn new(packet: &[u8], protocols: &[Protocol]) -> Headers {
         let ethernet = EthernetPacket::new(packet).unwrap();
         let mut ethertype = ethernet.get_ethertype();
         let mut payload = ethernet.payload().to_vec();
@@ -35,7 +70,7 @@ impl Nprint {
             ethertype = vlan_packet.get_ethertype();
             payload = vlan_packet.payload().to_vec();
         }
-        let (mut ipv4, mut tcp, mut udp) = if ethertype == EtherTypes::Ipv4 {
+        let (ipv4, tcp, udp) = if ethertype == EtherTypes::Ipv4 {
             let ipv4_packet = Ipv4Packet::new(&payload).unwrap();
             let ipv4 = Some(Ipv4Header::new(&ipv4_packet));
             let (tcp, udp) = match ipv4_packet.get_next_level_protocol() {
@@ -57,28 +92,34 @@ impl Nprint {
         } else {
             (None, None, None)
         };
-        if protocols.contains(&Protocol::Ipv4) && ipv4.is_none() {
-            ipv4 = Some(Ipv4Header::default());
+        Headers {
+            ipv4: if protocols.contains(&Protocol::Ipv4) {
+                if ipv4.is_some() {
+                    ipv4
+                } else {
+                    Some(Ipv4Header::default())
+                }
+            } else {
+                None
+            },
+            tcp: if protocols.contains(&Protocol::Tcp) {
+                if tcp.is_some() {
+                    tcp
+                } else {
+                    Some(TcpHeader::default())
+                }
+            } else {
+                None
+            },
+            udp: if protocols.contains(&Protocol::Udp) {
+                if udp.is_some() {
+                    udp
+                } else {
+                    Some(UdpHeader::default())
+                }
+            } else {
+                None
+            },
         }
-        if protocols.contains(&Protocol::Tcp) && tcp.is_none() {
-            tcp = Some(TcpHeader::default());
-        }
-        if protocols.contains(&Protocol::Udp) && udp.is_none() {
-            udp = Some(UdpHeader::default());
-        }
-        Nprint { ipv4, tcp, udp }
-    }
-    pub fn print(&self) -> Vec<f32> {
-        let mut output = vec![];
-        if let Some(ipv4) = &self.ipv4 {
-            output.extend(ipv4.get_data());
-        }
-        if let Some(tcp) = &self.tcp {
-            output.extend(tcp.get_data());
-        }
-        if let Some(udp) = &self.udp {
-            output.extend(udp.get_data());
-        }
-        output
     }
 }
