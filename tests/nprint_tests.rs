96cb90519239720aff69c660f2f56377d3f04ecb
Johann <johann.hugon@pm.me>
2025-04-13 08:45:43 -0500

    Fix fmt

--- tests/nprint_tests.rs
+++ tests/nprint_tests.rs
@@ -243,6 +243,10 @@ mod nprint_tests {
     fn test_nprint_no_ethernet() {
         let raw_packet = vec![0x0];
         let nprint = Nprint::new(&raw_packet, vec![ProtocolType::Ipv4]);
-        assert_eq!(nprint.print(), [-1.0; 480], "Expected default IPv4, find something else");
+        assert_eq!(
+            nprint.print(),
+            [-1.0; 480],
+            "Expected default IPv4, find something else"
+        );
     }
 }
